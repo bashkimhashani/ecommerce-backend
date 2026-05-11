0567dfed920c3d6535f991786c912e726ce3560a
Anis Millaku <anis.millaku@student.uni-pr.edu>
2026-05-11 18:23:53 +0200

    Update cart API files

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CartDetailView, CartItemCreateView
+from .views import CartDetailView, CartItemCreateView, CartItemDetailView
 
 
 urlpatterns = [
     path('', CartDetailView.as_view(), name='cart-detail'),
     path('items/', CartItemCreateView.as_view(), name='cart-item-list'),
+    path('items/<int:item_id>/', CartItemDetailView.as_view(), name='cart-item-detail'),
 ]
