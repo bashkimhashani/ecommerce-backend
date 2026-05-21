7b2a6d07695655fb6220d0b48f0bb917dcb3df02
shpetim.panduri@student.uni-pr.edu <shpetim.panduri@student.uni-pr.edu>
2026-05-21 16:30:44 +0200

    fix: resolve pytest failures in CI

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -76,7 +78,10 @@ class Cart(TenantModel):
 
     @property
     def subtotal(self):
-        return sum(item.line_total for item in self.items.all())
+        return sum(
+            (item.line_total for item in self.items.all()),
+            Decimal('0.00'),
+        )
 
     def save(self, *args, **kwargs):
         if self.tenant_id is None:
