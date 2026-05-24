1385c5e3fbb33971c3c62579282c7259f50cdca7
Anis Millaku <anis.millaku@student.uni-pr.edu>
2026-05-24 15:10:21 +0200

    chore: add retry backoff to external api tasks

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 
-@shared_task
+@shared_task(autoretry_for=(Exception,), retry_backoff=True)
 def send_order_confirmation(order_id):
     from orders.models import Order
 
@@ -49,7 +49,7 @@ def send_order_confirmation(order_id):
     }
 
 
-@shared_task
+@shared_task(autoretry_for=(Exception,), retry_backoff=True)
 def send_order_shipped(order_id):
     from orders.models import Order
 
