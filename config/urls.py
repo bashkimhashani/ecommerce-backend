fb2e7ee92c7de30c5679e59500466ad9739dfaf6
shpetim.panduri@student.uni-pr.edu <shpetim.panduri@student.uni-pr.edu>
2026-05-24 22:39:44 +0200

    fix: serve media files and update gitignore

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -30,3 +32,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(
         url_name='schema'), name='swagger-ui'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
